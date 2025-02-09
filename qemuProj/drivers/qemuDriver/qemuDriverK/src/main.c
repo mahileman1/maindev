23671e0f342a66e95bbdafb74e7ddd22fbee0308
mahileman1 <mahileman1@gmail.com>
2025-02-08 21:07:59 -0500

    Sample device and network working in VM, log updated.  Kernel mode in progress

--- qemuProj/drivers/qemuDriver/qemuDriverK/src/main.c
+++ qemuProj/drivers/qemuDriver/qemuDriverK/src/main.c
@@ -1,7 +1,6 @@
 // Finally, some code
 
 
-
 #include <ntddk.h>
 #include <wdf.h>
 
@@ -9,53 +8,51 @@
 DRIVER_INITIALIZE DriverEntry;
 EVT_WDF_DRIVER_DEVICE_ADD KmdfEvtQemuDeviceAdd;
 
-// Copy-pasted from MS docs for now
-
+// DriverObject -> Pointer to driver object
+// RegistryPath -> pointer to path (string) to driver-specific registry key
 NTSTATUS
 DriverEntry(
     _In_ PDRIVER_OBJECT     DriverObject,
     _In_ PUNICODE_STRING    RegistryPath
 )
 {
-    // NTSTATUS variable to record success or failure
     NTSTATUS status = STATUS_SUCCESS;
-
     // Allocate the driver configuration object
     WDF_DRIVER_CONFIG config;
 
-    // Print "Hello World" for DriverEntry
-    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
+    // Initialize the driver configuration with callback
+    WDF_DRIVER_CONFIG_INIT(&config, qemuEvtDeviceAdd);
 
-    // Initialize the driver configuration object to register the
-    // entry point for the EvtDeviceAdd callback, KmdfHelloWorldEvtDeviceAdd
-    WDF_DRIVER_CONFIG_INIT(&config,
-        KmdfEvtQemuDeviceAdd
-    );
-
-    // Finally, create the driver object
-    status = WdfDriverCreate(DriverObject,
+    status = WdfDriverCreate(
+        DriverObject,
         RegistryPath,
         WDF_NO_OBJECT_ATTRIBUTES,
         &config,
         WDF_NO_HANDLE
     );
+
     return status;
 }
 
+// EvtDeviceAdd is called by the framework in response to AddDevice
+// call from the PnP manager.Here the driver should register all the
+// PNP, power and Io callbacks, register interfaces and allocate other
+// software resources required by the device.The driver can query
+// any interfaces or get the config space information from the bus driver
+// but cannot access hardware registers or initialize the device.
+
 NTSTATUS
-KmdfEvtQemuDeviceAdd(
+qemuEvtDeviceAdd(
     _In_    WDFDRIVER       Driver,
     _Inout_ PWDFDEVICE_INIT DeviceInit
 )
 {
-    // We're not using the driver object,
-    // so we need to mark it as unreferenced
     UNREFERENCED_PARAMETER(Driver);
 
-    NTSTATUS status;
+    NTSTATUS status = STATUS_SUCCESS;
 
     // Allocate the device object
-    WDFDEVICE hDevice;
+    WDFDEVICE device;
 
     // Print "Hello World"
     KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
@@ -63,7 +60,7 @@ KmdfEvtQemuDeviceAdd(
     // Create the device object
     status = WdfDeviceCreate(&DeviceInit,
         WDF_NO_OBJECT_ATTRIBUTES,
-        &hDevice
+        &device
     );
     return status;
 }
