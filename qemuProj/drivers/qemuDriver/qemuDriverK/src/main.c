2ad0d83722716bf01d6d5a2a6a82a97c4fab268c
mahileman1 <mahileman1@gmail.com>
2025-02-13 21:18:40 -0500

    Some general progress, mostly kernel driver

--- qemuProj/drivers/qemuDriver/qemuDriverK/src/main.c
+++ qemuProj/drivers/qemuDriver/qemuDriverK/src/main.c
@@ -50,17 +50,61 @@ qemuEvtDeviceAdd(
     UNREFERENCED_PARAMETER(Driver);
 
     NTSTATUS status = STATUS_SUCCESS;
-
-    // Allocate the device object
     WDFDEVICE device;
 
-    // Print "Hello World"
-    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
+    WDF_PNPPOWER_EVENT_CALLBACKS pnpCallbacks;
+    WDF_OBJECT_ATTRIBUTES attributes;
+    // Specifying data buffer methods - can use buffered, undefined, max? 
+    WdfDeviceInitSetIoType(DeviceInit, WdfDeviceIoDirect);
 
-    // Create the device object
+    // Clean slate
+    WDF_PNPPOWER_EVENT_CALLBACKS_INIT(&pnpCallbacks);
+
+    // Prep HW - called after PnP has assigned HW resources
+    pnpCallbacks.EvtDevicePrepareHardware = qemuPrepHW;
+    // Release HW - called after framework has stopped sending I/O
+    // request, interrupts have been disabled, device has been turned off
+    pnpCallbacks.EvtDeviceReleaseHardware = qemuReleaseHW;
+
+    // D0 state when:
+    // - Device is enumerated (plugged in, system reboot)
+    // - Devices return from low-power state
+    // - PnP manager redistributes HW resources
+    pnpPowerCallbacks.EvtDeviceD0Entry = qemuD0Entry;
+    pnpPowerCallbacks.EvtDeviceD0Exit = qemuD0Exit;
+
+    WdfDeviceInitSetPnpPowerEventCallbacks(DeviceInit, &pnpCallbacks);
+    // PnP stuff set
+    // Clean slate
+    WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&attributes, DEVICE_EXTENSION);
+
+    // Took this from mic's example - TODO I don't understand this
+    // By opting for SynchronizationScopeDevice, we tell the framework to
+    // synchronize callbacks events of all the objects directly associated
+    // with the device. In this driver, we will associate queues and
+    // and DpcForIsr. By doing that we don't have to worrry about synchronizing
+    // access to device-context by Io Events and DpcForIsr because they would
+    // not concurrently ever. Framework will serialize them by using an
+    // internal device-lock.
+    //
+    attributes.SynchronizationScope = WdfSynchronizationScopeDevice;
+
+    attributes.EvtCleanupCallback = qemuCleanup;
+
     status = WdfDeviceCreate(&DeviceInit,
-        WDF_NO_OBJECT_ATTRIBUTES,
+        &attributes,
         &device
     );
+
+    PDEVICE_EXTENSION devExtension = qemuGetDeviceContext(device);
+    devExtensions->device = device;
+
+    status = WdfDeviceCreateDeviceInterface(
+        device,
+        /**/, // GUID? 
+        NULL
+    );
+
+
     return status;
 }
